741bd82f62d1a1ea2bfd74fc962857b8d51ab6a1
Shashank <34152717+iThink32@users.noreply.github.com>
2018-02-12 13:19:59 +0530

    Add files via upload

---
+++ GenericCache.swift
@@ -0,0 +1,28 @@
+//
+//  GenericCache.swift
+//  RynlyUI
+//
+//  Created by N A Shashank on 2/12/18.
+//  Copyright © 2018 N A Shashank. All rights reserved.
+//
+
+import UIKit
+
+public class GenericCache<cacheObjectType> where cacheObjectType:AnyObject {
+    
+    let cache = NSCache<NSString,cacheObjectType>()
+    
+    public func fetchObject(name:String) -> cacheObjectType? {
+        return cache.object(forKey: name as NSString)
+    }
+    
+    public func saveObject(obj1:cacheObjectType,name:String) {
+        self.cache.setObject(obj1, forKey: NSString(string:name))
+    }
+    
+    public init() {
+        // nothing to do here
+    }
+    
+    
+}
